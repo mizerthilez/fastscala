2ff23624a33c134b71ff687de12e867ff86ca4e7
K.X <xuxiang999@gmail.com>
2024-12-01 22:15:58 +0800

    extension: String.toOption

--- fastscala/src/main/scala/com/fastscala/utils/utils.scala
+++ fastscala/src/main/scala/com/fastscala/utils/utils.scala
@@ -2,3 +2,5 @@ package com.fastscala.utils
 
 given ContextFunc_2_NormalFunc_Conversion[A, B]: Conversion[A ?=> B, A => B] =
   contextFunc => contextFunc(using _)
+
+extension (s: String) def toOption: Option[String] = Some(s).filter(_.nonEmpty)
